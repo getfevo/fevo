2953a963e79c2735641a40893d7fdcb4272f31bf
Manuel <manuel2@noebauer.at>
2025-03-15 00:32:48 +0100

    feat: update middleware to customize session cookie handling and remove debugging logs

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,12 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
 export async function middleware(request: NextRequest) {
-	const session = getSessionCookie(request);
-
-	// Debugging Logs
-	console.log("Middleware triggered on:", request.url);
-	console.log("Session Cookie:", session);
-	console.log("Request Headers:", request.headers);
+	const session = getSessionCookie(request, {
+        // Optionally pass config if cookie name, prefix or useSecureCookies option is customized in auth config.
+		cookieName: "session_token",
+		cookiePrefix: "better-auth",
+    });
 
 	// Check if session exists
 	if (!session) {
