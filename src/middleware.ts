8c0331e2f4467c073a1a234234a20f0b89796afd
Manuel <manuel2@noebauer.at>
2025-03-15 09:46:09 +0100

    fix: update session cookie retrieval to use request headers directly

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
 export async function middleware(request: NextRequest) {
-	const cookies = getSessionCookie(request);
+	//const cookies = getSessionCookie(request.headers.get("cookie"));
+	const cookies = request.headers.get("cookie")
 	console.log(cookies);
 	if (!cookies) {
 		return NextResponse.redirect(new URL("/", request.url));
