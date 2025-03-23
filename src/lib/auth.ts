91dbe359a23e527dc1a4429585ede6e09105029b
Manuel <manuel2@noebauer.at>
2025-03-23 20:51:25 +0100

    feat: implement email verification with resend

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,7 @@ import { db } from "@/db";
 import { user, session, account, verification, member, invitation, organization } from "@/db/schema";
 import { nextCookies } from "better-auth/next-js";
 import { organization as orgPlugin } from "better-auth/plugins/organization";
+import { resend } from "@/lib/mail/resend";
 
 // Determine environment
 const isProduction = process.env.NODE_ENV === "production";
@@ -11,6 +12,7 @@ const domain = isProduction
     ? process.env.NEXT_PUBLIC_APP_URL 
     : process.env.NEXT_PUBLIC_DEV_URL || undefined;
 const secure = process.env.NEXT_PUBLIC_COOKIE_SECURE === "true" || isProduction;
+const from = process.env.BETTER_AUTH_FROM_EMAIL || "";
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
@@ -36,8 +38,17 @@ export const auth = betterAuth({
         }
     },
     emailAndPassword: {  
-        enabled: true
+        enabled: true,
+        requireEmailVerification: true,
     },
+    emailVerification: {
+        async sendVerificationEmail({ user, url }) {
+			const res = await resend.emails.send({
+                from: "manuel@getfevo.io",
+				to: user.email,
+				subject: "Verify your fevo email address",
+				html: `<a href="${url}">Verify your email address</a>`,
+          })}},
     socialProviders: {
         google: { 
             clientId: process.env.GOOGLE_CLIENT_ID as string, 
