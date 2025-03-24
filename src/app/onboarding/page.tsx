5f3ed4d5ab4a386b69d99709b95c1667ac69e7bb
harshsbhat <harsh121102@gmail.com>
2025-03-24 12:42:18 +0530

    fix: middleware fix to validate the session

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div style={{ display: "flex", height: "100vh", alignItems: "center", justifyContent: "center" }}>
+        <h1 style={{ fontSize: "2rem", fontWeight: "bold" }}>Onboarding! 🚀</h1>
+      </div>
+    );
+  }
+  
